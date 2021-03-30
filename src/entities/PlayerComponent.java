416582cb4b97fa948d1b9d00891a24c0bdc29fd5
lukasberk <lukasvdberk@gmail.com>
2021-03-30 20:51:34 +0200

    [Refactor] Split into multiple method

--- src/entities/PlayerComponent.java
+++ src/entities/PlayerComponent.java
@@ -3,10 +3,11 @@ package entities;
 import com.almasb.fxgl.core.math.Vec2;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.component.Component;
+import javafx.util.Duration;
 
 public class PlayerComponent extends Component {
-    public static final float BLOCK_MOVE_SPEED = 200F;
-    public static final float FALL_SPEED = 100F;
+    public static float BLOCK_MOVE_SPEED = 200F;
+    public static float FALL_SPEED = 100F;
 
     private int blockSize = 50;
 
@@ -17,33 +18,55 @@ public class PlayerComponent extends Component {
 
     @Override
     public void onUpdate(double tpf) {
-        // moves player forward
-        latestPlayerPos.x += (tpf * BLOCK_MOVE_SPEED);
-
-        if(isJumping) {
-            // jump the player
-            float amountJumpedInframe = (amountToJump * (float) tpf) * 2;
-            amountToJump -= amountJumpedInframe;
-            latestPlayerPos.y -= amountJumpedInframe;
-            if(amountToJump <= 4) {
-                isJumping = false;
-                latestPlayerPos.y = (float) (FXGL.getAppHeight() - blockSize);
-            }
-        }
-
-        if (latestPlayerPos.y >= FXGL.getAppHeight() - blockSize) {
-            // so the player does not fall of the map
-            latestPlayerPos.y = (float) (FXGL.getAppHeight() - blockSize);
-        }
-
-        else {
-            // to always pull the player downwards if it is not attached to the ground
-            latestPlayerPos.y += (tpf * FALL_SPEED);
-        }
+        movePlayerToRight(tpf);
+        updateY(tpf);
 
         entity.setX(latestPlayerPos.x);
         // translate Y will increase it exponentially do it won't work
         entity.setY(latestPlayerPos.y);
+    }
+
+    private void movePlayerToRight(double tpf) {
+        latestPlayerPos.x += (tpf * BLOCK_MOVE_SPEED);
+    }
+
+    private void updateY(double tpf) {
+        if(isJumping) {
+            jumpPlayerForGivenFrame(tpf);
+        }
+
+        if (touchesGround()) {
+            setPlayerToGround();
+        }
+
+        else {
+            pullPlayerDownWords(tpf);
+        }
+    }
+
+    private void jumpPlayerForGivenFrame(double tpf) {
+        // jump the player
+        float amountJumpedInframe = (amountToJump * (float) tpf) * 2;
+        amountToJump -= amountJumpedInframe;
+        latestPlayerPos.y -= amountJumpedInframe;
+
+        if(amountToJump <= 4) {
+            isJumping = false;
+            latestPlayerPos.y = (float) (FXGL.getAppHeight() - blockSize);
+        }
+    }
+
+
+    private boolean touchesGround() {
+        return latestPlayerPos.y >= FXGL.getAppHeight() - blockSize;
+    }
+
+    private void setPlayerToGround() {
+        latestPlayerPos.y = (float) (FXGL.getAppHeight() - blockSize);
+    }
+
+    private void pullPlayerDownWords(double tpf) {
+        latestPlayerPos.y += (tpf * FALL_SPEED);
     }
 
     public void jump() {
